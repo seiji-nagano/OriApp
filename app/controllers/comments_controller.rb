b2774c59ee665e934dab5792d1a06ccb73462151
seiji-nagano <144106275+seiji-nagano@users.noreply.github.com>
2023-10-24 15:03:35 +0900

    コメント編集機能の中止

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,23 +15,6 @@ class CommentsController < ApplicationController
     end
   end
 
-  def edit
-    @tweet = Tweet.find(params[:tweet_id])
-    @comment = Comment.find(params[:id])
-  end
-
-  def update
-    @tweet = Tweet.find(params[:post_id])
-    @comment = Comment.find(params[:id])
-    if @comment.update(comment_params)
-      flash[:success] = "Comment updated"
-      redirect_to @tweet
-    else
-      flash[:danger] = "Comment failed"
-      render 'edit'
-    end
-  end
-
   private
   def comment_params
     params.require(:comment).permit(:comment_content, :text, :video).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
